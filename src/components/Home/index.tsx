7f5e20dd42104de87edc4470486acfccf4b511a0
sKyun92 <lsk9105@gmail.com>
2021-02-07 16:59:33 +0900

    ADD Home 기능 구현

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import Button from '../common/Button';
+import { useKbtiDispatch } from '../context';
 
 function Home() {
+  const dispatch = useKbtiDispatch();
+  const history = useHistory();
+
+  const goToNextPage = () => {
+    dispatch({
+      type: 'NEXT_PAGE',
+    });
+    history.push('/question');
+  };
+
   return (
     <HomeWrapper>
       <h1>일제감정기 시절 나는 독립운동가? 친일파?</h1>
-      <Button>시작하기</Button>
+      <Button onClick={goToNextPage}>시작하기</Button>
     </HomeWrapper>
   );
 }
