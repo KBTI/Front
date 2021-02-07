44e343cf3aeff82121aec730f8b17b8c19bfe36a
sKyun92 <lsk9105@gmail.com>
2021-02-07 17:00:09 +0900

    ADD Result 기능 구현

--- src/components/Result/index.tsx
+++ src/components/Result/index.tsx
@@ -1,14 +1,45 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { RESULT_LIST } from '../../utils/constant';
+import Button from '../common/Button';
+import { useKbtiDispatch } from '../context';
 export interface ResultParamsType {
   id: string;
 }
 
 function Result() {
   const param = useParams<ResultParamsType>();
+  const history = useHistory();
+  const dispatch = useKbtiDispatch();
 
-  console.log(param);
-  console.log('gkdl');
-  return <div>Result{param.id}</div>;
+  const RESUlT = RESULT_LIST.find((item) => item.name === param.id);
+  console.log(RESUlT);
+  const handleReset = () => {
+    dispatch({
+      type: 'RESET',
+    });
+    history.push('/');
+  };
+  return (
+    <ResultWrapper>
+      <h3>{RESUlT.title}</h3>
+      <ul>
+        {RESUlT.contants.map((item, index) => {
+          return <li key={index}>{item}</li>;
+        })}
+      </ul>
+      <Button onClick={handleReset}>다시하기</Button>
+    </ResultWrapper>
+  );
 }
 export default Result;
+
+const ResultWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+
+  li {
+    margin-top: 1rem;
+  }
+`;
